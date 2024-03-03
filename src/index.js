bf6efd609e3c5b43f776c00c017da04b05d51a77
abdelrahman979797 <151799909+abdelrahman979797@users.noreply.github.com>
2024-03-03 04:48:51 +0200

    abdelrahman

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
-import '../node_modules/sweetalert2/dist/sweetalert2.all';
 // import Swal from '../node_modules/sweetalert2/dist/sweetalert2';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
